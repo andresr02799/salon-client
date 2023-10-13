9dd082ca3b8f873c64e2c707e554470d92c415c4
Andres Ramirez <andresr02799@gmail.com>
2023-10-13 00:23:30 -0500

    Rutas Login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import InputText from "../components/inputText";
 import Boton from "../components/botton";
-import {BrowserRouter as Redirect, useHistory} from "react-router-dom";
 
 function Login() {
-    
-        const history = useHistory()
-        function handleClick() {
-          history.push("/salones")
-    }
     return (
         <div class=" bg-midnight h-screen flex justify-center align-middle items-center">
             <div class="bg-whitetext p-8 rounded shadow-lg w-96">
@@ -16,7 +10,7 @@ function Login() {
                     <h1 class="font-bold text-[35px]">LOGIN</h1>
                     <InputText nameLabel="Correo*" typeInput="email" placeholder="Ingrese el correo electronico" />
                     <InputText nameLabel="Contrasena*" typeInput="password" placeholder="Ingrese la contrasena"/>
-                    <Boton nameBoton='INICIAR' onClick={() => handleClick()}/>
+                    <Boton nameBoton='INICIAR'/>
                 </form>
             </div>
         </div>
